f80952988a905082e0a9998406cbbe6554a59db0
Karl Amoguis <ktamoguis@gmail.com>
2018-05-07 07:10:31 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,8 @@ def create_project_hash
   projects = {}
 
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    #projects[project] = {}
+    projects[title:] = 
   end
 
   #return hash
